31323f98d5dec01fadedeff633bae7a287f27136
Ronja <ronja.bertsch@gmx.de>
2016-04-12 00:10:48 +0100

    Task 4

---
+++ src/assignment2/DropboxUser.java
@@ -0,0 +1,20 @@
+package assignment2;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+@PersistenceCapable
+public class DropboxUser {
+	
+	@PrimaryKey
+	@Persistent
+	private Key id;
+	
+	public DropboxUser (Key id){
+		
+		this.id = id;
+	}
+}
