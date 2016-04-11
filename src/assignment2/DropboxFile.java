a461e0d72f7720882bea0f88c2fcda794cf65cec
Ronja <ronja.bertsch@gmx.de>
2016-04-12 00:10:35 +0100

    Task 3

---
+++ src/assignment2/DropboxFile.java
@@ -0,0 +1,40 @@
+package assignment2;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.datastore.Key;
+
+@PersistenceCapable
+public class DropboxFile {
+	
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key id;
+	
+	@Persistent
+	private String name;
+	
+	@Persistent
+	private DropboxDirectory owner;
+	
+	@Persistent
+	private BlobKey blob;
+	
+	public DropboxFile (String name, BlobKey blob, DropboxDirectory owner){
+		this.name = name;
+		this.blob = blob;
+		this.owner = owner;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public BlobKey getBlobKey() {
+		return blob;
+	}
+}
